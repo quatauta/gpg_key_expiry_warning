5c3dbc461c910edb3725c3b19fe205f05fdb6924
Daniel Schömer <daniel.schoemer@gmx.net>
2019-06-16 19:49:25 +0200

    rspec executes in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,10 @@ require "bundler/setup"
 require "gpg_key_expiry_warning"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.example_status_persistence_file_path = ".rspec_status"
+  config.order = :random
+  Kernel.srand config.seed
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
